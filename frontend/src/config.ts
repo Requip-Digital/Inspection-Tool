2a3fe166a831a815f2db3830f3fb94ea32f9d605
Iron0utlaw <hvarshney123@gmail.com>
2025-05-31 19:56:27 +0530

    endpoint change

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   apiUrl: process.env.NODE_ENV === 'production'
-    ? 'https://requip-chi.vercel.app'
+    ? 'https://requip.onrender.com'
     : 'http://localhost:5001'
 }; 
